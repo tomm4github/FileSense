d674e1ad3dd297458a47beeec7dca8529edff5a9
tomm4github <tmorganarchitect@gmail.com>
2024-11-07 20:30:47 -0800

    New file

--- PDFIntel.py
+++ PDFIntel.py
@@ -7,7 +7,6 @@ def extract_metadata(pdf_path):
         metadata = pdf_reader.metadata
         return metadata
 
-
 if __name__ == '__main__':
     pdf_path = '/Users/tommorgan/Desktop/Desktop/Thomas.H.MorganCV_.pdf'  # Replace with your PDF file path
     metadata = extract_metadata(pdf_path)
